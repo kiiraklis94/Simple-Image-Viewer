5a7e9b0ab303c6374a41c5353e3df49f477c2c98
kiiraklis94 <kiiraklis94@gmail.com>
2016-03-04 17:20:59 +0200

    Image now gets scaled according to window size. Kinda slow though.

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,17 +1,15 @@
 import java.awt.EventQueue;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
+import java.awt.Image;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import java.awt.BorderLayout;
 import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 import java.awt.Color;
 import java.awt.Toolkit;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 public class MainWindow {
 
@@ -50,32 +48,45 @@ public class MainWindow {
 
 	private void initialize() {
 		
-		BufferedImage img = null;
+//		try{
+//			bImg = ImageIO.read(new File(imageLocation));
+//		}catch (IOException e) {
+//		    e.printStackTrace();
+//		}
 		
-		try{
-			img = ImageIO.read(new File(imageLocation));
-		}catch (IOException e) {
-		    e.printStackTrace();
-		}
-		int imgWidth = img.getWidth();
-		int imgHeight = img.getWidth();
+		ImageIcon icon = new ImageIcon(imageLocation);
+		Image img = icon.getImage();
+//		BufferedImage bImg = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);		
 		
-		frame1 = new JFrame();
 		
+		frame1 = new JFrame();		
 		frame1.setIconImage(Toolkit.getDefaultToolkit().getImage(MainWindow.class.getResource("/Icon/gifLogo.png")));
 		frame1.getContentPane().setBackground(Color.WHITE);
 		frame1.setTitle(imageLocation);
 		frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame1.getContentPane().setLayout(new BorderLayout(0, 0));
-		frame1.setBounds(200, 200, imgWidth+18, imgHeight+40);
+		frame1.setBounds(200, 200, img.getWidth(null), img.getHeight(null));
 
 		JLabel imgLabel = new JLabel("");
 		imgLabel.setToolTipText(imageLocation);
 		imgLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		imgLabel.setIcon(new ImageIcon(imageLocation));
-		imgLabel.setBounds(0, 0, frame1.getWidth(), frame1.getWidth());
+		imgLabel.setIcon(new ImageIcon(img));
+		imgLabel.setBounds(0, 0, frame1.getWidth(), frame1.getHeight());
 		frame1.getContentPane().add(imgLabel);
 		frame1.setLocationRelativeTo(null);
+		
+		frame1.addComponentListener(new ComponentAdapter() {
+			@Override
+			public void componentResized(ComponentEvent e) {
+				
+				imgLabel.setBounds(0, 0, frame1.getWidth(), frame1.getHeight());
+				Image newImg = img.getScaledInstance(frame1.getWidth(), frame1.getHeight(), java.awt.Image.SCALE_DEFAULT);
+//				Graphics gr = bImg.createGraphics();
+//				gr.drawImage(img, 0, 0, frame1.getWidth(), frame1.getHeight(), frame1);
+				
+				imgLabel.setIcon(new ImageIcon(newImg));
+			}
+		});
 		
 		
 	}
